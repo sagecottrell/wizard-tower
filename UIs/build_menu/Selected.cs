96070ad0f0c45bca4bfecddf43a67742fc029539
Sage <inquisitorpinkie@gmail.com>
2026-03-28 13:18:18 -0700

    input event on selected

--- UIs/build_menu/Selected.cs
+++ UIs/build_menu/Selected.cs
@@ -4,4 +4,11 @@ namespace wizardtower.UIs.build_menu;
 
 public partial class Selected : Node3D
 {
+    public void InputEvent(Node camera, InputEvent @event, Vector3 position, Vector3 normal, long shapeIdx)
+    {
+        if (@event is InputEventMouseButton mouseEvent && mouseEvent.Pressed)
+        {
+            GD.Print("Selected");
+        }
+    }
 }
