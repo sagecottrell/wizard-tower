48f049f45c393723fc0ea1cf08b7a237428cd7e8
sagecottrell <sage@cottrell.dev>
2026-08-26 13:12:03 -0700

    wip

--- events/event-gen.py
+++ events/event-gen.py
@@ -4,7 +4,7 @@ import re
 
 dir = pathlib.Path(__file__).parent
 
-filter_re = re.compile(r"([A-Z][a-z]+)([A-Z][a-z]+ing)([A-Z][a-z]+)*Event")
+filter_re = re.compile(r"^([A-Z][a-z]+)+([A-Z][a-z]+ed)Event.cs$")
 
 for event_file in dir.rglob("*Event.cs"):
     if not filter_re.match(event_file.name):
@@ -12,11 +12,15 @@ for event_file in dir.rglob("*Event.cs"):
     text = event_file.read_text()
 
     cls_name = event_file.name.removesuffix('.cs')
-    new_name = cls_name.replace("ing", "ed", count=1)
+    new_name = cls_name.replace("ed", "ing", count=1)
 
     text = text.replace(f'class {cls_name}', f'class {new_name}')
     text = text.replace(': BaseEvent, ', ': BaseEvent, IDeniableEvent, ')
     text = text.replace('{', '{\n    public bool IsAllowed { get; set; } = true;', count=1)
 
-    event_file.with_name(new_name + '.cs').write_text(text)
+    new_file = event_file.with_name(new_name + '.cs')
+    if new_file.exists():
+        continue
+    
+    new_file.write_text(text)
 
