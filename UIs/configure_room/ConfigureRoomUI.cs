f34ddff7b3c77dc9104d4792f696db38faf896ef
sagecottrell <sage@cottrell.dev>
2026-09-06 19:41:55 -0700

    clean up

--- UIs/configure_room/ConfigureRoomUI.cs
+++
@@ -1,9 +0,0 @@
-using Godot;
-using wizardtower.state;
-
-namespace wizardtower.UIs.configure_room;
-
-public partial class ConfigureRoomUI(TowerState tower, RoomState roomState) : CanvasLayer, IUserInterface
-{
-
-}
